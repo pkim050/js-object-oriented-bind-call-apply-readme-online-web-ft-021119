90eedc22d0d5d53ad281c5f63ed59e4122cc8ce7
Patrick Kim <pkim050@gmail.com>
2019-06-06 15:43:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 function greet() {
     console.log(`my name is ${this.name}, hi!`);
 }
- 
+
 greet(); // my name is , hi!
- 
+
 let person = {
     name: 'Bob',
     greet: greet
 };
- 
+
 person.greet();
